927671fbb2d4aae14d305bf6c16dde90d0896564
subin4123 <subindas4123@gmail.com>
2025-05-05 10:59:29 +0530

    third

--- my-node/app.js
+++ my-node/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline!');
+  res.send('New version deployed');
 });
 
 app.listen(port, () => {
